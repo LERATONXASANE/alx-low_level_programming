0b8569fe3e3b8deeb9c65004c9cd1d9a81fc910f
LERATONXASANE <leratonxasane@gmail.com>
2022-03-21 13:01:18 -0700

    adding file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  *Rturn: Always 0 (succes)
  */
-int main(void)
+int main(void);
 {
 char charType;
 int intType;
@@ -13,10 +13,10 @@ double doubleType;
 long int longIntType;
 float floatType;
 
-printf("Size of a char: %zu byte\n", sizeof(charType));
-printf("Size of an int: %zu bytes\n", sizeof(intType));
-printf("Size of a double: %zu byte\n", sizeof (doubleType));
-printf("Size of a long int: %zu byte\n", sizeof(longIntType));
-printf("Size of a float: %zu byte\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
+printf("Size of a double: %zu byte(s)\n", sizeof(doubleType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
