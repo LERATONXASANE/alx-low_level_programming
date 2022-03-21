4078b85d80e5787228c640949935cf54bfc27896
LERATONXASANE <leratonxasane@gmail.com>
2022-03-21 13:41:52 -0700

    adding file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ *Return: Always 1 (success)
+ */
+
+int main(void)
+{
+  printf("and tha piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+  return (1);
+}
