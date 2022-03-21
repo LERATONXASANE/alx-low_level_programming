00813c2db5f6e6e140f2a0b3f61f707aafe78af4
LERATONXASANE <leratonxasane@gmail.com>
2022-03-21 12:33:19 -0700

    adding file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
   char a[] = "\"Programming is like building a multilingual puzzle";
 
   puts(a);
-  return (0);  
+  return (0);
 }
