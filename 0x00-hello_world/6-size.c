bda323c31fbe8def5b0232befc27083989cf792c
LERATONXASANE <leratonxasane@gmail.com>
2022-03-21 13:04:48 -0700

    adding file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
- *main -Entry
+/**
+ *main - Entry
  *
- *Rturn: Always 0 (succes)
+ *Rturn: Always 0 (success)
  */
-int main(void);
+int main(void)
 {
 char charType;
 int intType;
