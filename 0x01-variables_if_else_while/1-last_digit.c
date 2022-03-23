df025f745539b8e42c2d778c20205b90ec5a3d7b
LERATONXASANE <leratonxasane@gmail.com>
2022-03-23 08:13:15 -0700

    adding file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,15 +20,15 @@ last_digit = n % 10;
 printf("last digit of %d is ", n);
 if (last_digit > 5)
 {
-printf("last_digit and is greater than 5\n");
+  printf("%d and is greater than 5\n", last_digit);
 }
 else if (last_digit == 0)
 {
-printf("last_digit and is 0\n");
+  printf("%d and is 0\n", last_digit);
 }
 else if (last_digit < 6 && last_digit != 0)
 {
-printf("last_digit and is less than 6 and not 0\n");
+  printf("%d and is less than 6 and not 0\n", last_digit);
 }
 return (0);
 }
