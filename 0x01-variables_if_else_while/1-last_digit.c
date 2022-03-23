fa008234a8f405599e06cf68698998be66d04715
LERATONXASANE <leratonxasane@gmail.com>
2022-03-23 08:04:00 -0700

    adding file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,8 +21,13 @@ if (last_digit > 5)
 {
 printf("and is greater than 5\n");
 }
-if (last_digit = 0)
+else if (last_digit == 0)
 {
 printf("and is 0\n");
 }
-else printf("and is less than 6 and not 0\n");
+else if (last_digit < 6 && last_digit != 0)
+{
+printf("and is less than 6 and not 0\n");
+}
+return (0);
+}
