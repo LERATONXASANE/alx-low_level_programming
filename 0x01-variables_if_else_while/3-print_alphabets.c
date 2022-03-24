3dabff3d760b7e9cb5938bc974c8b609e46d269d
LERATONXASANE <leratonxasane@gmail.com>
2022-03-24 04:15:14 -0700

    adding file 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - alphabet in lowercase, followed by a new line.
+ *main - alphabet in lowercase, followed by a new line.
  *
- * Always return(0)
+ *Return: Always return(0)
  */
 
 int main(void)
