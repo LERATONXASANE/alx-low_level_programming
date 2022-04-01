9aac1389db430ff7393842c411e048351ff3ec1e
LERATONXASANE <leratonxasane@gmail.com>
2022-04-01 18:05:52 +0000

    adding new files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function that prints positive or negative integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
